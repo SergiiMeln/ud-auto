305e9b10ad19e95ce8c26f69564bdd64e8a5d5fe
Sergii Melnychenko <sergii.melnychenko@epicgames.com>
2025-09-19 08:20:23 -0400

    add annotations

--- tests/example.test.ts
+++ tests/example.test.ts
@@ -7,29 +7,7 @@ test("Base Test Example", async ({ page }) => {
   await page.waitForTimeout(3000);
 });
 
-test("Click on Element", async ({ page }) => {
-  await page.goto("http://zero.webappsecurity.com/");
-  await page.click("#signin_button");
-  await page.click("text=Sign in");
-  const errMsg = page.locator(".alert-error");
-  await expect(errMsg).toContainText("Login and/or password are wrong.");
-  await page.waitForTimeout(3000);
-});
-
-test("Working with Inputs", async ({ page }) => {
-  await page.goto("http://zero.webappsecurity.com/");
-  await page.click("#signin_button");
-
-  await page.fill("#user_login", "my-username");
-  await page.fill("#user_password", "password");
-  await page.waitForTimeout(3000);
-
-  await page.click("text=Sign in");
-  const errMsg = page.locator(".alert-error");
-  await expect(errMsg).toContainText("Login and/or password are wrong.");
-});
-
-test("Test Assertions", async ({ page }) => {
+test.only("Test Assertions", async ({ page }) => {
   await page.goto("https://www.example.com");
   await expect(page).toHaveURL("https://www.example.com");
   await expect(page).toHaveTitle("Example Domain");
@@ -40,4 +18,28 @@ test("Test Assertions", async ({ page }) => {
   await expect(pageTitle).toHaveCount(1);
 
   await page.waitForTimeout(2000);
+});
+
+test.describe.only("Test Set", () => {
+  test("Working with Inputs", async ({ page }) => {
+    await page.goto("http://zero.webappsecurity.com/");
+    await page.click("#signin_button");
+
+    await page.fill("#user_login", "my-username");
+    await page.fill("#user_password", "password");
+    await page.waitForTimeout(3000);
+
+    await page.click("text=Sign in");
+    const errMsg = page.locator(".alert-error");
+    await expect(errMsg).toContainText("Login and/or password are wrong.");
+  });
+
+  test("Click on Element", async ({ page }) => {
+    await page.goto("http://zero.webappsecurity.com/");
+    await page.click("#signin_button");
+    await page.click("text=Sign in");
+    const errMsg = page.locator(".alert-error");
+    await expect(errMsg).toContainText("Login and/or password are wrong.");
+    await page.waitForTimeout(3000);
+  });
 });
