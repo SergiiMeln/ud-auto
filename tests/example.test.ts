b1ed1048116306339c2aa1c7337fcf9c687d4cfa
Sergii Melnychenko <sergii.melnychenko@epicgames.com>
2025-09-22 19:58:34 -0400

    add before each test block

--- tests/example.test.ts
+++ tests/example.test.ts
@@ -45,13 +45,15 @@ test.describe("Test Set @Regression", () => {
 });
 
 test.describe.only("Screenshots", () => {
-  test("Whole page screenshot", async ({ page }) => {
+  test.beforeEach(async ({ page }) => {
     await page.goto("http://zero.webappsecurity.com/");
+  });
+
+  test("Whole page screenshot", async ({ page }) => {
     await page.click("#signin_button");
     await page.screenshot({ path: "screenshot.png", fullPage: true });
   });
   test("Screenshot of an element", async ({ page }) => {
-    await page.goto("http://zero.webappsecurity.com/");
     await page.click("#signin_button");
     const signInButton = page.getByText("Sign in");
     await signInButton.screenshot({ path: "buttonScreenshot.png" });
