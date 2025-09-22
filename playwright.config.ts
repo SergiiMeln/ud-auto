9499ab10354f9ed8098707bdb425abf792b97179
Sergii Melnychenko <sergii.melnychenko@epicgames.com>
2025-09-22 08:32:15 -0400

    add pw config file

---
+++ playwright.config.ts
@@ -0,0 +1,30 @@
+import { PlaywrightTestConfig } from "@playwright/test";
+
+const config: PlaywrightTestConfig = {
+  timeout: 30000,
+  retries: 0,
+  use: {
+    headless: true,
+    viewport: { width: 1280, height: 720 },
+    actionTimeout: 15000,
+    ignoreHTTPSErrors: true,
+    video: "off",
+    screenshot: "off",
+  },
+  projects: [
+    {
+      name: "chrome",
+      use: { browserName: "chromium" },
+    },
+    {
+      name: "firefox",
+      use: { browserName: "firefox" },
+    },
+    {
+      name: "webkit",
+      use: { browserName: "webkit" },
+    },
+  ],
+};
+
+export default config;
